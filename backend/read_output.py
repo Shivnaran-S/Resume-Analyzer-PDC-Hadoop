550957041fab3402b2b030f2837905f4967240fa
Shivnaran-S <s.shivnaran@gmail.com>
2025-04-14 17:08:30 +0530

    Updated the code

--- backend/read_output.py
+++ backend/read_output.py
@@ -2,11 +2,19 @@
 from .utils import run_in_wsl
 
 def fetch_output():
-    output_hdfs_dir = "/resume_output/part-00000"#"/resume_project/output/part-r-00000"
-    local_output = "/mnt/e/Siva Naran/Sem 6/PDC/Package/Resume Analyzer PDC Hadoop/output/hadoop_output.txt"
+    hdfs_output = "/resume_output/part-00000"
+    linux_dir = "/home/hadoop/temp_output"
+    linux_output = f"{linux_dir}/hadoop_output.txt"
+    windows_output = "/mnt/e/Siva\\ Naran/Sem\\ 6/PDC/Package/Resume\\ Analyzer\\ PDC\\ Hadoop/output/"
 
-    # Get output from HDFS to local file
-    run_in_wsl(f"/home/hadoop/hadoop-3.3.6/bin/hdfs dfs -get -f {output_hdfs_dir} {local_output}")
+    # Ensure the target WSL directory exists
+    run_in_wsl(f"mkdir -p {linux_dir}")
 
-if __name__=="__main__":
+    # Download file to WSL native directory
+    run_in_wsl(f"/home/hadoop/hadoop-3.3.6/bin/hdfs dfs -get -f {hdfs_output} {linux_output}")
+
+    # Copy file from WSL to Windows path
+    run_in_wsl(f"cp {linux_output} {windows_output}")
+
+if __name__ == "__main__":
     fetch_output()
